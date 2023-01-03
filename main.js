ed276167faebfa001501644d93c815743ed98d35
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-01-03 21:07:08 +0200

    Use figma style

--- main.js
+++ main.js
@@ -2,9 +2,13 @@
 const menuIcon = document.querySelector('.menu-icon');
 const navMenu = document.querySelector('.nav-items');
 const navLinks = document.querySelectorAll('.nav-link');
+const blurs = document.getElementById('blur');
+// const blurs = document.querySelectorAll('.blur');
 menuIcon.addEventListener('click', () => {
   menuIcon.classList.toggle('active');
   navMenu.classList.toggle('active');
+  blurs.classList.toggle('blur');
+//   blurs.classList.create('active');
 });
 
 navLinks.forEach((navLink) => {
